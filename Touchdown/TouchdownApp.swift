4a053fe66630e2e9ef94a735716c2fe9cbff0f0e
Utkarsh Dayal <utkarshdayal.17@gmail.com>
2021-05-24 11:22:46 +0530

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Dayal, Utkarsh on 24/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
